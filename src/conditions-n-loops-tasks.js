5a8c59066ea7772903b5aedcfe510171ff986b50
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2024-01-27 01:42:05 +0100

    feat: add solution for spiral matrix

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -189,8 +189,15 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let j = str.length - 1;
+  for (let i = 0; i < str.length / 2; i += 1) {
+    if (str[i] !== str[j]) {
+      return false;
+    }
+    j -= 1;
+  }
+  return true;
 }
 
 /**
@@ -207,8 +214,13 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
@@ -226,8 +238,15 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let number = num;
+  while (number > 0) {
+    if (number % 10 === digit) {
+      return true;
+    }
+    number = Math.floor(number / 10);
+  }
+  return false;
 }
 
 /**
@@ -243,8 +262,24 @@ function isContainNumber(/* num, digit */) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  const size = arr.length;
+  const startIndex = 1;
+  for (let i = startIndex; i < size; i += 1) {
+    let leftSum = 0;
+    for (let j = i - 1; j >= 0; j -= 1) {
+      leftSum += arr[j];
+    }
+    let rightSum = 0;
+    for (let k = i + 1; k < size; k += 1) {
+      rightSum += arr[k];
+    }
+
+    if (leftSum === rightSum) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
@@ -268,8 +303,51 @@ function getBalanceIndex(/* arr */) {
  *          [10, 9,  8,  7]
  *        ]
  */
-function getSpiralMatrix(/* size */) {
-  throw new Error('Not implemented');
+function getSpiralMatrix(size) {
+  const matrix = [];
+
+  for (let i = 0; i < size; i += 1) {
+    matrix[i] = [];
+    for (let j = 0; j < size; j += 1) {
+      matrix[i][j] = 0;
+    }
+  }
+
+  let val = 1;
+
+  let sizeRow = size;
+  let sizeColumn = size;
+
+  let startRowIndex = 0;
+  let startColumnIndex = 0;
+
+  while (startRowIndex < sizeRow && startColumnIndex < sizeColumn) {
+    for (let i = startColumnIndex; i < sizeColumn; i += 1) {
+      matrix[startRowIndex][i] = val;
+      val += 1;
+    }
+    startRowIndex += 1;
+
+    for (let i = startRowIndex; i < sizeRow; i += 1) {
+      matrix[i][sizeColumn - 1] = val;
+      val += 1;
+    }
+    sizeColumn -= 1;
+
+    for (let i = sizeColumn; i > startColumnIndex; i -= 1) {
+      matrix[sizeRow - 1][i - 1] = val;
+      val += 1;
+    }
+    sizeRow -= 1;
+
+    for (let i = sizeRow; i > startRowIndex; i -= 1) {
+      matrix[i - 1][startColumnIndex] = val;
+      val += 1;
+    }
+    startColumnIndex += 1;
+  }
+
+  return matrix;
 }
 
 /**
